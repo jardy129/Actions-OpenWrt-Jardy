9cf4614c5535bd4addba05e6887e8e9e48eed0ff
jardy129 <32240587+jardy129@users.noreply.github.com>
2022-04-14 21:31:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,4 +13,3 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git luci-theme-edge https://github.com/garypang13/luci-theme-edge.git' >>feeds.conf.default
