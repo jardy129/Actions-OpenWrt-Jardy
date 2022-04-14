ce932be06cdc0e8e2f40717723e4492ba807c513
jardy129 <32240587+jardy129@users.noreply.github.com>
2022-04-14 21:20:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,6 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git luci-theme-edge https://github.com/kiddin9/luci-theme-edge.git' >>feeds.conf.default
-echo 'src-git serverchan https://github.com/tty228/luci-app-serverchan.git' >>feeds.conf.default
+echo 'src-git luci-theme-edge https://github.com/garypang13/luci-theme-edge.git' >>feeds.conf.default
